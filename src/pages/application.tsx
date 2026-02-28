590500a311890998c925dec31a50cb6ad44754a0
joschonarth <joschonarth@gmail.com>
2026-02-28 11:20:46 -0300

    feat: add auth token to API requests

--- src/pages/application.tsx
+++ src/pages/application.tsx
@@ -1,17 +1,12 @@
-import { useQuery } from '@tanstack/react-query'
 import { Loader2 } from 'lucide-react'
 import { CreateGoal } from '../components/create-goal'
 import { EmptyGoals } from '../components/empty-goals'
 import { Dialog } from '../components/ui/dialog'
 import { WeeklySummary } from '../components/weekly-summary'
-import { getSummary } from '../http/get-summary'
+import { useGetWeekSummary } from '../http/generated/api'
 
 export function Application() {
-  const { data, isLoading } = useQuery({
-    queryKey: ['summary'],
-    queryFn: getSummary,
-    staleTime: 1000 * 60, // 60 seconds
-  })
+  const { data, isLoading } = useGetWeekSummary()
 
   if (isLoading || !data) {
     return (
